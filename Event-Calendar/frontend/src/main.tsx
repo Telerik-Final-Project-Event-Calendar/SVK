255b753078cf6ec849b167a56f289aafea0e849e
savinaBrankova <savina.stancheva87@gmail.com>
2025-06-12 08:37:25 +0300

    try to fix the problem with the icon on the map in production

--- Event-Calendar/frontend/src/main.tsx
+++ Event-Calendar/frontend/src/main.tsx
@@ -1,7 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import shadow from 'leaflet/dist/images/marker-shadow.png';
+
+L.Marker.prototype.options.icon = L.icon({
+  iconRetinaUrl: iconRetina,
+  iconUrl: icon,
+  shadowUrl: shadow,
+});
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
